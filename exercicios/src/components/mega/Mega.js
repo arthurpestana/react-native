3423dd3bfa69f1ca59bb96bda109f899a3fec643
Arthur Henrique <arthurschneider355@gmail.com>
2024-02-26 22:05:21 -0300

    Componente Mega

--- exercicios/src/components/mega/Mega.js
+++ exercicios/src/components/mega/Mega.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Text, TextInput} from 'react-native'
+import {Button, Text, TextInput, View} from 'react-native'
 import estilo from '../estilo'
+import MegaNumero from './MegaNumero'
 
 export default class Mega extends React.Component {
 
@@ -8,12 +9,37 @@ export default class Mega extends React.Component {
         super(props) 
 
         this.state = {
-            qntnumeros: props.qntnumeros
+            qntnumeros: props.qntnumeros,
+            numeros: [],
         }
     }
 
-    altQuant(quant) {
-        this.setState({qntnumeros: quant})
+    altQuant = (qntnumeros) => {
+        this.setState({qntnumeros: +qntnumeros})
+    }
+
+    randomNumber(qntnumeros, numbers=[]) {
+        let rand = Math.floor(Math.random() * 60) + 1
+        if (numbers.includes(rand)) {
+            return this.randomNumber(qntnumeros, numbers)
+        }
+        numbers.push(rand)
+        if (qntnumeros<=1){
+            return numbers
+        }
+        return this.randomNumber(qntnumeros-1, numbers)
+    }
+
+    gerarNumeros = () => {
+        const numeros = this.randomNumber(this.state.qntnumeros)
+        this.setState({numeros: numeros})
+    }
+
+    exibirNumeros = () => {
+        const nums = this.state.numeros.sort((a,b)=>a-b)
+        return nums.map(num => {
+            return <MegaNumero key={num} num={num} />
+        })
     }
 
     render() {
@@ -21,15 +47,27 @@ export default class Mega extends React.Component {
             <React.Fragment>
                 <Text style={estilo.fontG}>
                     Gerador de Mega-Sena
-                    {this.state.qntnumeros}
                 </Text>
                 <TextInput 
                     keyboardType={'numeric'}
                     style={{borderBottomWidth: 2}}
                     placeholder='Quantidade de Números'
-                    value={this.state.qntnumeros}
-                    onChangeText={quant => this.altQuant(quant)}
+                    value={`${this.state.qntnumeros}`}
+                    onChangeText={qntnumeros => this.altQuant(qntnumeros)}
                 />
+                <Button title='Gerar Números' onPress={this.gerarNumeros}/>
+                <Text>
+                    Quantidade de Números Gerados: {this.state.qntnumeros}
+                </Text>
+                {this.state.numeros}
+                <View style={{
+                    flexDirection: 'row', 
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                }}>  
+                    {this.exibirNumeros()}  
+                </View>
+                
             </React.Fragment>
         )
     }
