558d998d173c8490522922e94d72a7cce9d90aa0
Arthur Henrique <arthurschneider355@gmail.com>
2024-03-08 22:34:45 -0300

    campo-minado

--- desafios/campo-minado/App.js
+++ desafios/campo-minado/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {SafeAreaView, View, Text, TouchableOpacity} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import styles from './src/styles/style_app'
+
+export default class App extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <SafeAreaView>
+
+            </SafeAreaView>
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ desafios/campo-minado/src/styles/style_app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create ({
+    container_app: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    },
+})
